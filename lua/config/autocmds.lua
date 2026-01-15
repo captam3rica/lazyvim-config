bc7e75ca5a62d88e8a1362964c6dcbf76c8a9d2f
captam3rica <matt.wilson@kandji.io>
2026-01-15 11:29:54 -0500

    json tap length to 2

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -42,3 +42,14 @@ vim.api.nvim_create_autocmd("FileType", {
         vim.opt_local.shiftwidth = 2
     end,
 })
+
+-- JSON
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = { "json", "jsonc", "json5" },
+    callback = function()
+        vim.opt_local.expandtab = true
+        vim.opt_local.tabstop = 2
+        vim.opt_local.softtabstop = 2
+        vim.opt_local.shiftwidth = 2
+    end,
+})
